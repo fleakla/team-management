c63bd6f310fd71015a5742e830be11befea5fd0d
fleakla <fleakla@gmail.com>
2015-08-20 16:24:47 -0500

    adds the ability to list all existing teams and to add a new team

--- src/main/java/teammanagement/controllers/TeamController.java
+++ src/main/java/teammanagement/controllers/TeamController.java
@@ -1,9 +1,13 @@
 package teammanagement.controllers;
 
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import teammanagement.entities.Team;
 import teammanagement.services.teams.TeamService;
 
 /**
@@ -18,6 +22,15 @@ public class TeamController {
 
     @RequestMapping(value = "/team")
     public String index(Model model) {
+        model.addAttribute("teams", teamService.findAll());
+        return "team/index";
+    }
+
+    @RequestMapping(value = "/team/add", method = RequestMethod.POST)
+    public String addTeam(Model model, @RequestParam String name, @RequestParam String city, @RequestParam String sport) {
+
+        teamService.add(Lists.newArrayList(new Team(name, city, sport)));
+
         model.addAttribute("teams", teamService.findAll());
         return "team/index";
     }
